be9a92a7288171559cd02ecb25606d5338af1092
Travis Thomsen <travis.n.thomsen@gmail.com>
2015-05-13 13:31:45 -0500

    Add rspec test for wordpess::plugin and wordpress::theme.

---
+++ spec/defines/wordpress_plugins_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe 'wordpress::wp_plugin' do
+  let  :default_params do
+    {
+      :zip_url        => 'https://downloads.wordpress.org/plugin/custom-content-shortcode.2.1.3.zip',
+      :zip_filename   => 'custom-content-shortcode.2.1.3.zip',
+      :install_dir => '/opt/wordpress',
+      :wp_owner       => 'apache',
+      :wp_group       => 'apache',
+    }
+  end
+
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        context "With default parameters" do
+          let(:title) { 'custom-content-shortcode'}
+          let :params do
+            default_params
+          end
+
+          it do
+            should contain_exec('download-wordpress-plugin-custom-content-shortcode').with({
+              :command => "/usr/bin/wget -nc https://downloads.wordpress.org/plugin/custom-content-shortcode.2.1.3.zip"
+            })
+          end
+
+          it do
+            should contain_exec('extract-wordpress-plugin-custom-content-shortcode').with({
+              :command => "unzip ./custom-content-shortcode.2.1.3.zip"
+            })
+          end
+        end
+      end
+    end
+  end
+end
